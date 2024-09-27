903a198c8287f3c13d0322b2bee2ac0cf4a4075f
João Victor Lima <victtorlima15@gmail.com>
2024-09-27 20:29:55 -0300

    Development of the responsive style of the 450px screen and correction of the style of other dimensions

--- src/components/IntroductoryContent.tsx
+++ src/components/IntroductoryContent.tsx
@@ -9,36 +9,37 @@ import LinkSection from "./LinkSection";
 const IntroductoryContent = () => {
   return (
     <>
-      <section className="grid grid-cols-2 auto-cols-max gap-y-12 msto:grid-cols-col2 gap-x-7 msto:gap-x-20 mst:gap-x-28 msto:gap-y-12 max-w-sec m-m-centro">
+      <section className="grid grid-cols-col2b md:grid-cols-2 auto-cols-max mt:gap-y-8 md:gap-0 lg:gap-y-12 msto:grid-cols-col2 mt:gap-x-2 md:gap-x-7 msto:gap-x-20 mst:gap-x- max-w-sec m-m-centro">
         <div className="w-full msto:w-420 msto:w-480">
-          <h2 className="font-merriweather_sans font-bold lg:text-xl text-orange mt-20">
+          <h2 className="font-merriweather_sans font-bold mt:text-sm md:text-base lg:text-xl text-orange mt-20 md:mb-2">
             O MELHOR PARA SUAS PLANTAS
           </h2>
-          <h1 className="font-merriweather_sans font-bold md:text-[2.125rem] lg:text-40px mst:text-48px text-grayBlue mb-3 msto:mb-7">
+          <h1 className="font-merriweather_sans font-bold mt:text-34px md:text-36px lg:text-48px text-grayBlue mb-3 lg:mb-7">
             Ajudamos você a ter um lindo e saudável jardim.
           </h1>
-          <p className="font-merriweather_sans font-light md:text-[15px] lg:text-lg text-grayBlue mb-4 lg:mb-7">
-            Aprenda rapidinho a cuidar das suas verdinhas!
+          <p className="font-merriweather_sans font-light mt:text-sm md:text-[15px] lg:text-lg text-grayBlue mb-4 lg:mb-5 msto:mb-7 ">
+            Aprenda a cuidar das suas verdinhas!
           </p>
           <Link
             href={"/#dicas"}
-            className="inline-block font-merriweather_sans font-bold text-white	px-4 py-3 bg-gradient-to-t from-lime-500 to-lime-400 rounded-full after:content-arrow after:pl-4"
+            className="inline-block font-merriweather_sans font-bold text-white mb-3 mt:px-2 mt:py-2	md:px-4 md:py-3 bg-gradient-to-t from-lime-500 to-lime-400 rounded-full after:content-arrow after:mt:pl-2 after:md:pl-4 mt:mr-2 md:mr-3 lg:mr-7"
           >
             Aprender
           </Link>
-          <button className="ml-3 lg:ml-7 font-merriweather_sans font-bold text-grayBlue md:text-sm">
+          <button className="font-merriweather_sans font-bold text-grayBlue text-sm">
             <Image
               src={play}
-              width={49}
-              height={49}
+              width={40}
+              height={40}
               alt="ícone de play"
               sizes="100vw"
-              className="inline-block mr-3 lg:w-14 lg:h-14"
+              className="inline-block mr-3 md:w-12  lg:w-13 lg:h-13"
+              priority={true}
             ></Image>
             Assista ao vídeo
           </button>
         </div>
-        <div className="grid lg:grid-rows-row2 msto:grid-cols-col2-1 items-start">
+        <div className="grid lg:grid-rows-row2 msto:grid-cols-col2a items-start">
           <Image
             src={p_plant}
             alt="imagem de um exemplar da Dieffenbachia Seguine ou Comigo-ninguém-pode"
