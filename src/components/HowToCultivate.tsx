df267624abfec47078709f6c1672b160cdf5f674
João Victor Lima <victtorlima15@gmail.com>
2024-10-09 09:07:31 -0300

    creation and styling of the BtnTypePlant component, creation of the chosenTip and focus functions

--- src/components/HowToCultivate.tsx
+++ src/components/HowToCultivate.tsx
@@ -1,18 +1,30 @@
 import React from "react";
 import CardTips from "./CardTips";
+import BtnTypePlant from "./BtnTypePlant";
 
 const HowToCultivate = () => {
   const type = ["folhagens", "suculentas"];
 
   return (
     <>
-      <section>
+      <section className="howToCultivate">
         <h1 className="font-madimi_one text-2xl mt:text-[30px] md:text-36px lg:text-40px  text-grayBlue leading-normal pb-2 rounded-lg underline underline-offset-[12px] decoration-[#AAE83F] mb-10">
           Dicas Práticas para Cuidar de suas Plantas
         </h1>
-        {type.map((categ) => (
-          <CardTips key={categ} categ={categ} />
-        ))}
+        <div>
+          <BtnTypePlant
+            bg="bg-gradient-to-t from-[#499b7f] to-[#6AAC96]"
+            categ="folhagens"
+            typePlant="Folhagens"
+          />
+          <BtnTypePlant
+            bg="bg-gradient-to-t from-lime-500 to-lime-400"
+            categ="suculentas"
+            typePlant="Suculentas"
+          />
+        </div>
+        <CardTips key={type[0]} categ={type[0]} display="grid" />
+        <CardTips key={type[1]} categ={type[1]} display="hidden" />
       </section>
     </>
   );
